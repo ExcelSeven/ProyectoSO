5e285f510611fbc0a7d6284b8db053ec3c9b69a4
juancgar <juangarza373@gmail.com>
2020-06-14 15:43:14 -0500

    added the parser to the project

--- Simulador.py
+++ Simulador.py
@@ -4,10 +4,15 @@ import fnmatch
 
 # sys.argv[] has the arguments from a python command line 
 pattern = re.compile(r'(.)*(.txt)')
+parser = re.compile(r'P \d+ \d+|A \d+ \d+ \d|L \d+|C \w+|F[^A-Za-z]|E$')
 if (len(sys.argv) != 2):
     print("argumentos incorrectos")
 else:
     #revisa si el archivo no es txt
     if(pattern.search(sys.argv[1]) == None):
         print('el texto no es un .txt')
-
+    else:
+        file_Object = open(sys.argv[1],'r')
+        data = file_Object.read()
+        lista = parser.findall(data)
+        print(lista)
