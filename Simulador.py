75670fd7f6e58992f2f6677e67cb688596b8a4d2
juancgar <juangarza373@gmail.com>
2020-06-14 14:10:27 -0500

    added the sys library and the regex for validating txt files

--- Simulador.py
+++ Simulador.py
@@ -1,3 +1,13 @@
 import sys
+import re
+import fnmatch
 
 # sys.argv[] has the arguments from a python command line 
+pattern = re.compile(r'(.)*(.txt)')
+if (len(sys.argv) != 2):
+    print("argumentos incorrectos")
+else:
+    #revisa si el archivo no es txt
+    if(pattern.search(sys.argv[1]) == None):
+        print('el texto no es un .txt')
+
