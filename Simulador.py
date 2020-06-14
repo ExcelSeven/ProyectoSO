06df68242c1c947849849b9f445a0f547451fa6c
Carrr105 <carlosger_64@hotmail.com>
2020-06-14 14:52:00 -0500

    a1

--- Simulador.py
+++ Simulador.py
@@ -1,5 +1,6 @@
 import sys
 from parser import parser
+import funciones #funciones seria un archivo a parte con las funciones
 
 if (sys.argv[1] not in ['fifo', 'lru'] or len(sys.argv) < 2):
     print ('Para la ejecucion se necesita un argumento, ademas de que este sea valido (fifo o lru)). Cerrando programa')
@@ -8,18 +9,21 @@ print ('Estrategia a usar: ', sys.argv[1])
 instruccionesParser = parser()
 # estrategia es una variable booleana que almacena estrategia. false es lru y true es fifo
 if sys.argv[1]=='fifo':
-    instrucciones.estrategia = True
+    #funciones seria un archivo a parte con las funciones
+    funciones.estrategia = True
 for instruccion in instruccionesParser:
+   #print("\n", ' '.join(str(s) for s in instruccion), sep="")
     if instruccion[0] == 'P':
-        instrucciones.P(instruccion[1], instruccion[2])
+        funciones.P(instruccion[1], instruccion[2])
     elif instruccion[0] == 'A':
-        instrucciones.A(instruccion[1], instruccion[2], instruccion[3])
+        funciones.A(instruccion[1], instruccion[2], instruccion[3])
     elif instruccion[0] == 'L':
-        instrucciones.L(instruccion[1])
+        funciones.L(instruccion[1])
     elif instruccion[0] == 'F':
-        instrucciones.F()
+        funciones.F()
     elif instruccion[0] == 'E':
-        instrucciones.E()
+        funciones.E()
     elif instruccion[0] != 'C':
         print("Instrucción no valida. Fin del programa")
         exit()
+
